59335a68f5a90961cf7c16a5e6365afbb4dc2f7e
sheryak_n <nathanraysheryak@gmail.com>
2022-08-14 09:16:32 -0400

    rebuild app w/ sqlalchemy, cat routes + model

---
+++ tree/extensions.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# we could check for os here and then change which db we use
+
+db = SQLAlchemy()
---
+++ tree/models/cat.py
@@ -0,0 +1,11 @@
+from tree.extensions import db
+
+
+class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    size = db.Column(db.String(20), nullable=False)
+    color = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f'<User id={self.id} name={self.name}>'
