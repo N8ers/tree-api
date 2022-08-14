81ba47e7dcee8451ec60db6f3146398731f77439
sheryak_n <nathanraysheryak@gmail.com>
2022-08-14 15:12:45 -0400

    move blueprint registration into function

--- tree/__init__.py
+++ tree/__init__.py
@@ -4,19 +4,19 @@ from tree.extensions import db
 
 
 def create_app():
-    # Create and configure app
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-    # Init database
     db.init_app(app)
 
     # Migrations here?
 
-    # Import Blueprints
-    from tree.blueprints import cat
-
-    # Register Blueprints / endpoints
-    app.register_blueprint(cat.blueprint)
+    register_blueprints(app)
 
     return app
+
+
+def register_blueprints(app):
+    from tree.blueprints import cat
+
+    app.register_blueprint(cat.blueprint)
