e1a73a134ef910e3704f6e172b6a921573882769
sheryak_n <nathanraysheryak@gmail.com>
2022-08-23 07:40:14 -0400

    connect db + migration

--- README.md
+++ README.md
@@ -12,7 +12,10 @@
   - enter venv
   - `flask --app tree --debug run`
 
-- Initilize the db `flask --app tree init-db`
+- Initilize the db `flask --app tree db init`
+- Create migration `flask --app tree db migrate -m "migrate name"`
+- Migrate up `flask --app tree db upgrade`
+- Migrate help menu `flask db --help`
 
 - Run tests `pytest`
 - Run tests with coverage `coverage run -m pytest` then `coverage report`
--- tree/__init__.py
+++ tree/__init__.py
@@ -1,9 +1,22 @@
+import sqlite3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def create_app():
 
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(80), unique=True, nullable=False)
+
+        def __repr__(self):
+            return '<User %r>' % self.username
 
     @app.route("/")
     def allo_world():
